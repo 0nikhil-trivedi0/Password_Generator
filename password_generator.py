8fdcf17ee84f67b4b19b59b90b01e4139912bb7a
00Herodotus00 <151948161+00Herodotus00@users.noreply.github.com>
2024-06-01 20:20:49 +0530

    Initial Commit.

---
+++ password_generator.py
@@ -0,0 +1,30 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_list = []
+
+for i in range(nr_letters):
+    x = letters[random.randint(0, 26)]
+    random_list.append(x)
+
+for i in range(nr_numbers):
+    x = numbers[random.randint(0, 9)]
+    random_list.append(x)
+
+for i in range(nr_symbols):
+    x = symbols[random.randint(0, 8)]
+    random_list.append(x)
+
+print(random_list)
+
+random.shuffle(random_list)
+string = "".join(random_list)
+print(string)
